2b7aba807a2092a7c43dbec234a6a3df5491cb95
Hirula <hirulapinibinda01@gmail.com>
2025-09-13 13:27:01 +0530

    Initializing SpringBoot

---
+++ src/main/java/com/seproject/lakgamana_backend/LakgamanaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seproject.lakgamana_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakgamanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakgamanaBackendApplication.class, args);
+	}
+
+}
